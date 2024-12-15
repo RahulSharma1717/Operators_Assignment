9e30488e3a4d0706bbb89cbcba699a4f811fb127
Rahul Sharma <96397148+RahulSharma1717@users.noreply.github.com>
2024-12-15 16:42:29 +0530

    Add files via upload

---
+++ Additional_Assignment_4.py
@@ -0,0 +1,15 @@
+# Write a Program where output is value raised to the power 100.
+
+# Input the base value
+base = int(input("Enter the base value: "))
+
+# Calculate the base raised to the power of 100
+result = base ** 100
+
+# Display the result
+print(f"The value of {base} raised to the power 100 is: {result}")
+
+
+# Output:
+"""Enter the base value: 2
+The value of 2 raised to the power 100 is: 1267650600228229401496703205376"""
